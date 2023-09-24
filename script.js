5e6c0f1ada973a15a9501b7c8e5096cde3b01930
Krzysztof <krzysieq95.mail@gmail.com>
2023-09-24 19:27:29 +0200

    Add load product from API and styles for product card

---
+++ script.js
@@ -0,0 +1,53 @@
+class StoreAPI {
+	constructor() {
+		this.endpoint = "https://fakestoreapi.com/";
+		this.lastId = 0;
+		this.initListeners();
+	}
+
+	initListeners() {
+		const seeAll = document.querySelector('#see-all');
+		seeAll.addEventListener('click', (evt) => {
+			evt.preventDefault();
+			if(!this.data) return;
+			this.renderProducts(this.data, 6);
+			seeAll.remove();
+		});
+	}
+
+	getProducts() {
+		fetch(this.endpoint + `products`)
+		.then(res => res.json()).then(data => {
+			this.data = data;
+			document.querySelector('.loading').classList.add('hidden');
+			this.renderProducts(this.data);
+		});
+	}
+
+	renderProducts(data, limit = 3) {
+		const productsContainer = document.querySelector('.products');
+		let countLimit = this.lastId+limit;
+		for(let i = this.lastId; i < countLimit && i < data.length; i++) {
+			if(data[i]) {
+				productsContainer.innerHTML += `
+				<div class="product-item">
+					<div class="product-image">
+						<img src="${data[i].image}" alt="${data[i].title}" />
+					</div>
+					<div class="product-content">
+						<span class="product-name">${data[i].title}</span>
+						<div class="product-info">
+							<span>$${data[i].price}</span>
+							<div class="divider"></div>
+							<span>${data[i].rating.rate} <i class="fi fi-rr-star"></i></span>
+						</div>
+					</div>
+				</div>`;
+				this.lastId = i;
+			}
+		}
+	}
+}
+
+const API = new StoreAPI();
+API.getProducts();
